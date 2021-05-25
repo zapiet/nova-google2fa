9640d4183d3a9ef5572dee38fb9190e29a8b56cf
Andrew Cargill <cargix1@gmail.com>
2021-05-25 16:20:58 +0100

    latest

--- src/Google2FAAuthenticator.php
+++ src/Google2FAAuthenticator.php
@@ -18,8 +18,7 @@ class Google2FAAuthenticator extends Authenticator
         return
             !$this->isEnabled() ||
             $this->noUserIsAuthenticated() ||
-            $this->twoFactorAuthStillValid() ||
-            $this->google2FAValidUntillNow();
+            $this->twoFactorAuthStillValid();
     }
 
     /**
@@ -34,14 +33,5 @@ class Google2FAAuthenticator extends Authenticator
         }
 
         return $secret;
-    }
-
-    protected function google2FAValidUntillNow()
-    {
-        $user2faTimeout = env('GOOGLE_2FA_TIMEOUT', (31*24*60*60));
-        $user2faValidUntill = is_object($this->getUser()->user2fa->updated_at) ? Carbon::parse($this->getUser()->user2fa->updated_at->timestamp)->addSeconds($user2faTimeout) : null;
-        $user2faValid = is_object($user2faValidUntill) && Carbon::now()->timestamp < $user2faValidUntill->timestamp;
-
-        return $user2faValid;
     }
 }
